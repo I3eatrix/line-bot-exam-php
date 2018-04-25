e840e81f432569bab5bf3ea8c6125cbe512cfbd5
I3eatrix <38729598+I3eatrix@users.noreply.github.com>
2018-04-25 22:22:45 +0700

    Update webhooks.php
    Change Channel access token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '9DzvGr4dMBCjheq7vI/5W3ip4p38J28zsjjDPrwJLVBXKdIKR1WxDVk7EykrjaWZn/xrcKfZ9plsI1qeI/Xneg54ZBaYs08NiSUlx9wvjxe7qa/FS9E8NZ4MFd6xC67OOfmbVR+vwt4Lt5uLm3pmUwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
